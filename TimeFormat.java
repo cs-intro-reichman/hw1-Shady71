90fe944183a0a5219ac8c8d1ca52cad763f140ed
Shady Gara <shadygara@h-MacBook-Pro-sl-Shady.local>
2024-11-13 00:20:28 +0200

    check

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,14 +3,16 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 
-		if (hours <= 12) {
+		if (hours < 12) {
 			System.out.println(hours + ":" + args[0].charAt(3) + args[0].charAt(4) + " AM");
+		} else {
+			if (hours == 12) {
+				System.out.println(hours + ":" + args[0].charAt(3) + args[0].charAt(4) + " PM");
+			}
+			else {
+				System.out.println(hours - 12 + ":" + args[0].charAt(3) + args[0].charAt(4) + " PM");
+			}
 		}
-		else {
-			System.out.println(hours - 12 + ":" + args[0].charAt(3) + args[0].charAt(4) + " PM");
-		}
-
-
 	}
 }
 
